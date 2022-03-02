70af7ce439c5c2c464b6bc1b547763b505f1027d
MoritzReufsteck <moritz.reufsteck@freudenberg.com>
2022-03-02 14:12:53 +0100

    added main and provider

---
+++ main.tf
@@ -0,0 +1,28 @@
+resource "aws_instance" "example" {
+  ami           = "ami-0eb7496c2e0403237"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+	description = "Allow tcp connection from everywhere"
+  }
+  
+  ingress {
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port   = 8
+  to_port     = 0
+  protocol    = "icmp"
+  description = "Allow ping from everywhere"
+  }
+}
